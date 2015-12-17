6696b403dfb4218c0087716c34a69274a90d5c0a
Fabien Potencier <fabien.potencier@gmail.com>
2015-12-17 22:41:51 +0100

    added support for multiple files in blackfire-player

--- Player/Console/PlayerCommand.php
+++ Player/Console/PlayerCommand.php
@@ -15,6 +15,7 @@ use Blackfire\Client as BlackfireClient;
 use Blackfire\ClientConfiguration as BlackfireClientConfiguration;
 use Blackfire\Player\Player;
 use Blackfire\Player\Loader\YamlLoader;
+use Blackfire\Player\ScenarioSet;
 use GuzzleHttp\Client as GuzzleClient;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -28,18 +29,12 @@ use Symfony\Component\Console\Logger\ConsoleLogger;
  */
 final class PlayerCommand extends Command
 {
-    public function __construct()
-    {
-
-        parent::__construct();
-    }
-
     protected function configure()
     {
         $this
             ->setName('run')
             ->setDefinition([
-                new InputArgument('file', InputArgument::REQUIRED, 'The YAML file defining the scenarios'),
+                new InputArgument('files', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'The files defining the scenarios'),
                 new InputOption('concurrency', 'c', InputOption::VALUE_REQUIRED, 'The number of client to create', 1),
                 new InputOption('endpoint', '', InputOption::VALUE_REQUIRED, 'Override the scenario endpoint', null),
                 new InputOption('output', 'o', InputOption::VALUE_REQUIRED, 'Saves the extracted values', null),
@@ -55,10 +50,6 @@ EOF
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (!is_file($file = $input->getArgument('file'))) {
-            throw new \InvalidArgumentException(sprintf('File "%s" does not exist.'));
-        }
-
         $logger = new ConsoleLogger($output);
 
         $clients = [$this->createClient()];
@@ -77,22 +68,29 @@ EOF
             $player->addExtension(new \Blackfire\Player\Extension\BlackfireExtension($blackfire, $logger));
         }
 
-        $loader = new YamlLoader();
-        $scenarios = $loader->load(file_get_contents($file));
-
         $variables = array();
         foreach ($input->getOption('variables') as $variable) {
             list($key, $value) = explode('=', $variable, 2);
             $variables[$key] = $value;
         }
 
-        foreach ($scenarios as $scenario) {
-            if ($input->getOption('endpoint')) {
-                $scenario->endpoint($input->getOption('endpoint'));
+        $loader = new YamlLoader();
+        $scenarios = new ScenarioSet();
+        foreach ($input->getArgument('files') as $file) {
+            if (!is_file($file)) {
+                throw new \InvalidArgumentException(sprintf('File "%s" does not exist.', $file));
             }
 
-            foreach ($variables as $key => $value) {
-                $scenario->value($key, $value);
+            foreach ($loader->load(file_get_contents($file)) as $reference => $scenario) {
+                if ($input->getOption('endpoint')) {
+                    $scenario->endpoint($input->getOption('endpoint'));
+                }
+
+                foreach ($variables as $key => $value) {
+                    $scenario->value($key, $value);
+                }
+
+                $scenarios->add($scenario, $reference);
             }
         }
 
