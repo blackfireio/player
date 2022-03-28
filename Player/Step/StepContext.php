46f042bf01ecb85d5aa9df522eae1d2d17d81b0c
Fabien Potencier <fabien@potencier.org>
2022-03-28 13:27:43 +0200

    Fix deprecation notice in player

--- Player/Step/StepContext.php
+++ Player/Step/StepContext.php
@@ -34,7 +34,7 @@ final class StepContext
 
     public function update(ConfigurableStep $step, array $variables)
     {
-        $this->workingDir = rtrim(\dirname($step->getFile()), '/').'/';
+        $this->workingDir = $step->getFile() ? rtrim(\dirname($step->getFile()), '/').'/' : '';
 
         if (null !== $step->getWait()) {
             $this->wait = $step->getWait();
