178f73edd705df2ed3e0d0046e091d7053e8d23c
Romain Neutron <romain@neutron.io>
2023-01-05 12:38:34 +0100

    Run Player PHP test suite on PHP 8.1 only

--- Player/Tests/Console/ScenarioHydratorTest.php
+++ Player/Tests/Console/ScenarioHydratorTest.php
@@ -48,7 +48,149 @@ EOF;
         $hydrator = new ScenarioHydrator();
         $scenarios = $hydrator->hydrate($input);
 
-        $expected = <<<EOEXPECTED
+        if (\PHP_VERSION_ID >= 80100) {
+            $expected = <<<EOEXPECTED
+Blackfire\Player\ScenarioSet {
+  -scenarios: [
+    Blackfire\Player\Scenario {
+      #next: null
+      -name: "'Visitor'"
+      -file: null
+      -line: 8
+      -errors: []
+      -auth: null
+      -headers: []
+      -wait: null
+      -json: null
+      -followRedirects: null
+      -blackfire: null
+      -blackfireRequest: null
+      -blackfireScenario: null
+      -samples: null
+      -warmup: null
+      -blockStep: Blackfire\Player\Step\VisitStep {
+        #next: Blackfire\Player\Step\VisitStep {
+          #next: null
+          -name: ""
+          -file: null
+          -line: 12
+          -errors: []
+          -auth: null
+          -headers: []
+          -wait: null
+          -json: null
+          -followRedirects: null
+          -blackfire: null
+          -blackfireRequest: null
+          -blackfireScenario: null
+          -samples: null
+          -warmup: null
+          -expectations: []
+          -variables: []
+          -assertions: []
+          -dumpValuesName: []
+          -uri: "url('/integrations')"
+          -method: null
+          -parameters: []
+          -body: null
+        }
+        -name: ""
+        -file: null
+        -line: 11
+        -errors: []
+        -auth: null
+        -headers: []
+        -wait: null
+        -json: null
+        -followRedirects: null
+        -blackfire: null
+        -blackfireRequest: null
+        -blackfireScenario: null
+        -samples: null
+        -warmup: null
+        -expectations: []
+        -variables: []
+        -assertions: []
+        -dumpValuesName: []
+        -uri: "url('/pricing')"
+        -method: null
+        -parameters: []
+        -body: null
+      }
+      -variables: [
+        "endpoint" => "'http://localhost'"
+        "user_login" => "'escargot'"
+        "user_password" => ""pwdsoupe""
+      ]
+      -endpoint: "'http://localhost'"
+      -key: null
+    }
+    Blackfire\Player\Scenario {
+      #next: null
+      -name: "'Authenticated'"
+      -file: null
+      -line: 14
+      -errors: []
+      -auth: null
+      -headers: []
+      -wait: null
+      -json: null
+      -followRedirects: null
+      -blackfire: null
+      -blackfireRequest: null
+      -blackfireScenario: null
+      -samples: null
+      -warmup: null
+      -blockStep: Blackfire\Player\Step\VisitStep {
+        #next: null
+        -name: ""
+        -file: null
+        -line: 17
+        -errors: []
+        -auth: null
+        -headers: []
+        -wait: null
+        -json: null
+        -followRedirects: null
+        -blackfire: null
+        -blackfireRequest: null
+        -blackfireScenario: null
+        -samples: null
+        -warmup: null
+        -expectations: []
+        -variables: []
+        -assertions: []
+        -dumpValuesName: []
+        -uri: "url('/login')"
+        -method: null
+        -parameters: []
+        -body: null
+      }
+      -variables: [
+        "endpoint" => "'http://localhost'"
+        "user_login" => "'escargot'"
+        "user_password" => ""pwdsoupe""
+      ]
+      -endpoint: ""http://tiptop.endpoint.minicontroll""
+      -key: null
+    }
+  ]
+  -keys: [
+    "" => true
+  ]
+  -extraBag: Blackfire\Player\ValueBag {
+    -values: []
+  }
+  -name: ""BKF Scenarios""
+  -variables: [
+    "endpoint" => "'http://localhost'"
+    "user_login" => "'escargot'"
+    "user_password" => ""pwdsoupe""
+  ]
+}
+EOEXPECTED;
+        } else {
+            $expected = <<<EOEXPECTED
 Blackfire\Player\ScenarioSet {
   -scenarios: [
     Blackfire\Player\Scenario {
@@ -188,6 +330,7 @@ Blackfire\Player\ScenarioSet {
   ]
 }
 EOEXPECTED;
+        }
 
         $this->assertSame($expected, $this->getVarDumperDump($scenarios));
     }
