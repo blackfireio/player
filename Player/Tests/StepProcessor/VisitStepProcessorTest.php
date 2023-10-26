78390bdd753ca6147e35fc9e4378e3c48da851aa
Marek Nocoń <mnocon@users.noreply.github.com>
2023-10-26 14:19:50 +0200

    Refactored testProcessPreserveHeaders to use a data provider
    Signed-off-by: Julien Jean <julien.jean@platform.sh>

--- Player/Tests/StepProcessor/VisitStepProcessorTest.php
+++ Player/Tests/StepProcessor/VisitStepProcessorTest.php
@@ -97,13 +97,14 @@ class VisitStepProcessorTest extends TestCase
         $this->assertSame(['foo' => 'bar'], $nextStep->getRequest()->body);
     }
 
-    public function testProcessPreserveHeaders()
+    /** @dataProvider provideForTestProcessPreserveHeaders */
+    public function testProcessPreserveHeaders(string $headerName, string $headerValue)
     {
         $processor = $this->createProcessor();
         $scenarioContext = new ScenarioContext('"foo"', new ScenarioSet());
 
         $step = new VisitStep('"http://localhost"');
-        $step->header('"Authorization: Bearer foo:bar"');
+        $step->header(sprintf('"%s: %s"', $headerName, $headerValue));
         $stepContext = new StepContext();
         $stepContext->update($step, []);
 
@@ -111,7 +112,13 @@ class VisitStepProcessorTest extends TestCase
         $this->assertCount(1, $nextSteps);
         $nextStep = $nextSteps[0];
         $this->assertInstanceOf(RequestStep::class, $nextStep);
-        $this->assertSame('Bearer foo:bar', $nextStep->getRequest()->headers['authorization'][0]);
+        $this->assertSame($headerValue, $nextStep->getRequest()->headers[strtolower($headerName)][0]);
+    }
+
+    public function provideForTestProcessPreserveHeaders(): iterable
+    {
+        yield 'Authorization header' => ['Authorization', 'Bearer foo:bar'];
+        yield 'Content-Type header' => ['Content-Type', 'application/custom'];
     }
 
     private function createProcessor(): StepProcessorInterface
