f753c321f8807e9cf0df3763c421fe6b65314e77
Julien Jean <julien.jean@platform.sh>
2024-11-06 08:58:16 +0000

    [player] ensure step is marked as errored when profile could not be performed as expected

--- Player/Extension/BlackfireExtension.php
+++ Player/Extension/BlackfireExtension.php
@@ -190,7 +190,10 @@ final class BlackfireExtension implements NextStepExtensionInterface, StepExtens
 
             $uuid = $response->request->headers[self::HEADER_BLACKFIRE_PROFILE_UUID][0];
             if (empty($response->headers[self::HEADER_BLACKFIRE_RESPONSE])) {
-                throw new LogicException('Are you authorized to profile this page? Probe not found or invalid signature. Please read https://support.blackfire.platform.sh/hc/en-us/articles/4843027173778-Are-You-Authorized-to-Profile-this-Page-Probe-Not-Found-or-Invalid-signature-');
+                $probeNotFoundOrInvalidSignatureErrorMessage = 'Are you authorized to profile this page? Probe not found or invalid signature. Please read https://support.blackfire.platform.sh/hc/en-us/articles/4843027173778-Are-You-Authorized-to-Profile-this-Page-Probe-Not-Found-or-Invalid-signature-';
+                $step->addError($probeNotFoundOrInvalidSignatureErrorMessage);
+
+                throw new LogicException($probeNotFoundOrInvalidSignatureErrorMessage);
             }
 
             // Check if the profile needs more samples
